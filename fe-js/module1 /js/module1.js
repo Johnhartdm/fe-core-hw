5a8f4549955d455b4b8045bbfcfd3d129b9b0cdf
Johnhartdm <ek9specr@gmail.com>
2018-02-16 00:08:55 +0200

    1

---
+++ fe-js/module1 /js/module1.js
@@ -0,0 +1,19 @@
+const firstString = 'Why you so crazy';
+const secondString = 'Just tell the truth';
+const thirdString = 'We will trust you';
+
+console.log(firstString.length);
+console.log(secondString.length);
+console.log(thirdString.length);
+
+const firstNumb = 16;
+const secondNumb = 17;
+const thirdNumb = 18;
+
+console.log(firstString.charAt(0),firstString.charAt(15));
+console.log(secondString.charAt(0),secondString.charAt(18));
+console.log(thirdString.charAt(0),thirdString.charAt(16));
+
+const result = `firstString length = ${firstNumb}, secondString length= ${secondNumb}, thirdString length = ${thirdNumb}`;
+
+console.log(result);
